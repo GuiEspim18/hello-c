ad1cbf1f6459a334ba538af3b6af8b129c520a64
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-03-01 14:01:01 -0300

    compare string

--- user-and-email.c
+++ user-and-email.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
+
+void cleanBuffer(char c) {
+    while((c = getchar()) != '\n' && c != EOF);
+}
 
 int main() {
 
     char email[50] ;
     char password[50] ;
 
+    // variable to clean buffer
+    char c;
+
     printf("Digite o seu email: ");
     scanf("%49[^\n]", email);
+
+    // cleaning buffer
+    cleanBuffer(c);
 
     printf("Digite a sua senha: ");
     scanf("%49[^\n]", password);
 
-    if (email == "user@gmail.com" && password == "123") {
+    // strcmp compare strings
+    if (strcmp(email, "user@gmail.com") == 0 && strcmp(password, "123") == 0) {
         printf("Olá user!");
     } else {
         printf("Email ou senha incorretos!");
